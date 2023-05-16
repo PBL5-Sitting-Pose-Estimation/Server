eb285b1123ebc5d4e936f47c374f861bdc2b96e0
Azanamin <minhnhat912002@gmail.com>
2023-05-16 20:27:00 +0700

    debug

--- utils/constants.js
+++ utils/constants.js
@@ -1,6 +1,6 @@
 const constants = {
     MONGO_DB_CONNECTION_STRING: 'mongodb+srv://pbl5:pbl5@pbl5.hcs5oix.mongodb.net/hpe?retryWrites=true&w=majority',
-    PORT: 8080
+    PORT: 8080|process.env.PORT
 }
 
 module.exports = constants
